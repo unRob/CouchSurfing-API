1716db5fef2ef847072b5f018b1f657f0e4c216d
rob <rob@partidosurrealista.com>
2012-06-12 18:58:34 -0500

    Moving to ruby
    Using the new CS API at api.couchsurfing.org
    The PHP "API" was moved to /php, will probably continue it sometime in
    the future

---
+++ csapi.rb
@@ -0,0 +1,141 @@
+#!/usr/bin/env ruby
+#encoding: utf-8
+
+=begin
+
+Licensed under The MIT License
+Copyright (c) 2012 Partido Surrealista Mexicano  
+=end
+
+require 'httparty'
+require 'json'
+#require 'pp'
+class CS
+  include HTTParty
+  base_uri 'https://api.couchsurfing.org'
+  headers "Content-Type" => 'application/json'
+  follow_redirects false
+  @uid = '0'
+  
+  
+  def initialize(username, password)
+    @username = username;
+    r = self.class.post('/sessions', body:{username:username, password:password}.to_json)
+    raise CS::AuthError.new("Could not login") if r.code != 200
+    @cookies = []
+    r.headers['Set-Cookie'].split(/, (?!\d)/).each do |cookie|;
+      key,value = cookie.split(';')[0].split('=')
+      @cookies = "#{key}=#{value}";
+    end
+    data = JSON.parse r.body
+    @uid = data['url'].gsub(/[^\d]/, '')
+    @profile = data.keep_if {|k,v| ['realname','username','profile_image','gender','address'].include?(k)}   
+    @profile['uid'] = @uid
+    self.class.headers 'Cookie' => @cookies
+    @@instance = self
+  end
+  
+  def CS::instance
+    @@instance
+  end
+  
+  def requests(limit=10)
+    url = "/users/#{@uid}/couchrequests"
+    q = {
+      limit: limit
+    }
+    r = self.class.get(url,query:q)
+    requests = {}
+    response = JSON.parse r.body
+    response['object'].each do |req|
+      key = req.gsub(/[^\d]/, '')
+      requests[key] = self.request(key)
+    end
+    requests
+  end
+  
+  
+  def request(id)
+    url = "/couchrequests/#{id}"
+    r = self.class.get(url)
+    JSON.parse r.body
+  end
+  
+  def userdata
+    @profile
+  end
+  
+  def profile(user=@uid)
+    url = "/users/#{user}/profile"
+    r = self.class.get(url)
+    JSON.parse r.body
+  end
+  
+  def photos(user=@uid)
+    url = "/users/#{user}/photos"
+    r = self.class.get(url)
+    JSON.parse r.body
+  end
+  
+  def friends(user=@uid)
+    url = "/users/#{user}/friends"
+    r = self.class.get(url)
+    JSON.parse r.body
+  end
+  
+  def references(user=@uid)
+    url = "/users/#{user}/references"
+    r = self.class.get(url)
+    JSON.parse r.body
+  end
+  
+  
+  class AuthError < StandardError
+  end
+  
+  class APIError < StandardError
+  end
+  
+  class Request
+    @api = nil
+    
+    def initialize(options={})
+      api = nil
+      if options[:username] && options[:password]
+        api = CS.new(options[:username], options[:password])
+        options.del(:username)
+        options.del(:password)
+      else
+        api = CS::instance
+        if api==nil
+          raise CS::APIError('You have not authenticated with the service or did not provide a :username and :password')
+        end
+      end
+      
+      #pp api.userdata
+      options[:subject] = options[:subject] || "#{api.userdata['realname']} from #{api.userdata['address']['country']} sent you a new CouchRequest!"
+      options[:number] = options[:number] || 1
+      options[:arrival_flexible] = options[:arrival_flexible] || false
+      options[:departure_flexible] = options[:departure_flexible] || false
+      options[:is_open_couchrequest] = options[:is_open_couchrequest] || false
+      options[:from] = api.userdata['uid']
+      options[:to] = options[:to] || api.userdata['uid']
+      options[:arrival] = Time.at(options[:arrival]).strftime("%FT%TZ") || (Time.now()+86400).strftime("%FT%TZ")
+      options[:departure] = Time.at(options[:departure]).strftime("%FT%TZ") || (Time.now()+86400*3).strftime("%FT%TZ")
+      options[:message] = options[:message] || "I'm to lazy to write a proper couch request. HOST ME PLZ?"
+      #puts options.to_json
+      
+      url = "/couchrequests"
+      response = api.post(url, body:options.to_json)
+      
+      #pp response.code
+      #pp response.body
+      
+    end
+    
+    
+    
+    
+  end
+  
+end
---
+++ example.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+#encoding: utf-8
+
+require './csapi.rb'
+
+begin
+  api = CS.new('username','password')
+rescue CS::AuthError
+  puts "Incorrect username or password"
+  exit;
+end
+
+# ===
+# Get a user's info, by default ours
+# ===
+profile = api.profile('205974')
+
+# ===
+# Get a user's photos, by default ours
+# ===
+photos = api.photos('205974')
+
+# ===
+# Get a user's friends, by default ours
+# ===
+friends = api.friends('205974')
+
+# ===
+# Get a user's references, by default ours
+# ===
+references = api.references('205974')
+
+# ===
+# Get the current user's recent requests
+# ===
+limit = 10
+requests = api.requests(limit)
+
+# ===
+# Create a new Couch Request
+# ===
+details = {
+  subject: 'This is my request subject',
+  number: 1, #How much people travel with you
+  arrival: 1339543920, #a Unix Timestamp with your arrival date
+  departure: 1339643920, #a Unix Timestamp with your departure date
+  arrival_flexible: true,
+  departure_flexible: false,
+  is_open_couchrequest: false,
+  to: 12345, #a numeric user id, I guess, have yet to figure this out ,
+  message: 'This is my request message' #I've yet to figure out how to do the multi-part requests
+}
+couch_request = CS::Request.new(details)
+
+#api.requests(1).each do |key, value|
+#  pp value
+#end
