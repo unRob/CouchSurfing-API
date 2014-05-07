0c917fe4bb43eea79d721f625880c49101298a04
rob <rob@partidosurrealista.com>
2014-05-07 02:51:51 -0500

    restructuring request

--- lib/csapi/request.rb
+++ lib/csapi/request.rb
@@ -3,38 +3,66 @@ module CS
   class Request
     @api = nil
 
-    def initialize(options={})
-      if options[:username] && options[:password]
-        api = CS.new(options[:username], options[:password])
-        options.del(:username)
-        options.del(:password)
-      else
-        api = CS::instance
-        if api==nil
-          raise CS::APIError('You have not authenticated with the service or did not provide a :username and :password')
-        end
+    attr_accesor :subject, :number, :arrival_flexible, :departure_flexible, :is_open_couchrequest, :from, :to, :arrival, :departure, :message
+
+    @options = [:subject, :number, :arrival_flexible, :departure_flexible, :is_open_couchrequest, :from, :to, :arrival, :departure, :message]
+
+    @defaults = {
+      subject: "A ruby programmer wants to surf your couch!",
+      number: 1,
+      arrival_flexible: false,
+      departure_flexible: false,
+      is_open_couchrequest: false,
+      from: nil,
+      to: nil,
+      arrival: Time.now,
+      departure: (Time.now+3600),
+      message: "I'm to lazy to write a proper couch request. HOST ME PLZ?"
+    }
+
+
+    def initialize options={}
+      options = @defaults.merge(options)
+      options.each do |k,v|
+        self.send "@#{k}=", val
       end
+    end
 
-      #pp api.userdata
-      options[:subject] = options[:subject] || "#{api.userdata['realname']} from #{api.userdata['address']['country']} sent you a new CouchRequest!"
-      options[:number] = options[:number] || 1
-      options[:arrival_flexible] = options[:arrival_flexible] || false
-      options[:departure_flexible] = options[:departure_flexible] || false
-      options[:is_open_couchrequest] = options[:is_open_couchrequest] || false
-      options[:from] = api.userdata['uid']
-      options[:to] = options[:to] || api.userdata['uid']
-      options[:arrival] = Time.at(options[:arrival]).strftime("%FT%TZ") || (Time.now()+86400).strftime("%FT%TZ")
-      options[:departure] = Time.at(options[:departure]).strftime("%FT%TZ") || (Time.now()+86400*3).strftime("%FT%TZ")
-      options[:message] = options[:message] || "I'm to lazy to write a proper couch request. HOST ME PLZ?"
-      #puts options.to_json
 
-      url = "/couchrequests"
-      api.post(url, body:options.to_json)
+    def arrival= date
+      raise ArgumentError.new("This does not seem like a Time instance") unless date.is_a? Time
+      @arrival = date
+    end
 
-      #pp response.code
-      #pp response.body
+
+    def departure= date
+      raise ArgumentError.new("This does not seem like a Time instance") unless date.is_a? Time
+      @departure = date
+    end
+
+
+    def to_h
+      params = {}
+      @options.each {|key| params[key.to_sym] = instance_variable_get("@#{key}") }
+      params
+    end
+
+
+    def send!
+      raise CS::APIError('You have not authenticated with the service or did not provide a :username and :password') unless CS.instance
+
+      data = self.to_h
+      data[:arrival] = data[:arrival].strftime("%FT%TZ")
+      data[:departure] = data[:departure].strftime("%FT%TZ")
+
+      me = CS.instance.userdata['uid']
+      data[:from] ||= me
+      data[:to] ||= me
+
+      CS::HTTP.instance.post('/couchrequests', body: data)
 
     end
+
   end
   
 end
